fda7a7154af479b84c31e56a95342156c1ab9eff
Finbar <finbar@finbar.xyz>
2022-10-08 20:13:27 -0400

    fixed potential path bug

--- src/Utils/DownloadURI.ts
+++ src/Utils/DownloadURI.ts
@@ -4,6 +4,7 @@ import * as fs from 'fs';
 export function downloadURI(url: string, path: string): Promise<void> {
   return new Promise((resolve) => {
     fetch(url).then(res => {
+      if(!fs.existsSync(path)) fs.mkdirSync(path, { recursive: true });
       const dest = fs.createWriteStream(path);
       res.body.pipe(dest);
       res.body.on('end', () => {
