64a2b0a2db85b19de7d6a8f1825f74bcd66f5c27
Finbar <finbar@finbar.xyz>
2022-10-11 23:46:48 -0400

    added temp directory check

--- src/Utils/DownloadURI.ts
+++ src/Utils/DownloadURI.ts
@@ -3,6 +3,7 @@ import * as fs from 'fs';
 
 export function downloadURI(url: string, path: string): Promise<void> {
     return new Promise((resolve) => {
+        if(!fs.existsSync('temp')) fs.mkdirSync('temp');
         fetch(url).then(res => {
             const dest = fs.createWriteStream(path);
             res.body.pipe(dest);
